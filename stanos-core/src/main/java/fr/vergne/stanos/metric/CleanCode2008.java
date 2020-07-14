7073da15e9137921d25aaf88864e79230133d2a6
Matthieu Vergne <matthieu.vergne@gmail.com>
2020-07-14 20:12:09 +0200

    wip

---
+++ stanos-core/src/main/java/fr/vergne/stanos/metric/CleanCode2008.java
@@ -0,0 +1,14 @@
+package fr.vergne.stanos.metric;
+
+/**
+ * Metrics defined in the book "Clean Code" written by Robert C. Martin (ISBN 13
+ * 978-0-13-235088-4).
+ * 
+ * TODO p.17+ (PDF 48)
+ * 
+ * TODO After this book, read "Agile Software Development: Principles, Patterns,
+ * and Practices"
+ */
+public class CleanCode2008 {
+
+}
